333f516bcf5f618cedb1f7a2ec322e7e0ee2d29a
Christoph Stach <christoph.stach@gmail.com>
2022-04-15 18:59:35 +0200

    WIP

--- app/components/TimEntryRow.tsx
+++ app/components/TimEntryRow.tsx
@@ -37,7 +37,7 @@ export function TimEntryRow(props: TimeEntryRowProps) {
                 })}>
                     <updateFetcher.Form method="post">
                         <input type="hidden" name="id" value={timeEntry.id} />
-                        <TextInput name="text" defaultValue={timeEntry.text} />
+                        <TextInput name="text" placeholder="Beschreibung hinzufügen" defaultValue={timeEntry.text} />
                     </updateFetcher.Form>
                 </Box>
 
