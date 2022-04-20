41ef671cd11feccd0d2bc1c87c527636aa7c6a9e
Christoph Stach <christoph.stach@gmail.com>
2022-04-20 16:20:50 +0200

    Add tabs

--- app/routes/__mainLayout/calendar/index.tsx
+++ app/routes/__mainLayout/calendar/index.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@mantine/core';
+import { Card, Tabs } from '@mantine/core';
 import type { DataFunctionArgs } from '@remix-run/node';
 import { authenticator } from '~/services/auth.server';
 import { forbidden } from 'remix-utils';
@@ -6,6 +6,7 @@ import { db } from '~/services/db.server';
 import { useLoaderData, useNavigate } from '@remix-run/react';
 import type { CalendarEntry } from '~/components/calendar';
 import CalendarWeekView from '~/components/calendar/CalendarWeekView';
+import CalendarMonthView from '~/components/calendar/CalendarMonthView';
 
 
 export async function loader({ request }: DataFunctionArgs) {
@@ -51,17 +52,34 @@ export default function () {
 
     return (
         <Card shadow="sm" p="md">
-            <CalendarWeekView
-                dayStartHour={8}
-                dayEndHour={20}
-                onEntryClick={handleEntryClick}
-                entries={loaderData.timeEntries.map((entry) => {
-                    return {
-                        ...entry,
-                        title: entry.text,
-                    }
-                })}
-            />
+            <Tabs>
+                <Tabs.Tab label="Woche">
+                    <CalendarWeekView
+                        dayStartHour={8}
+                        dayEndHour={20}
+                        onEntryClick={handleEntryClick}
+                        entries={loaderData.timeEntries.map((entry) => {
+                            return {
+                                ...entry,
+                                title: entry.text,
+                            }
+                        })}
+                    />
+                </Tabs.Tab>
+
+                <Tabs.Tab label="Monat">
+                    <CalendarMonthView
+                        onEntryClick={handleEntryClick}
+                        entries={loaderData.timeEntries.map((entry) => {
+                            return {
+                                ...entry,
+                                title: entry.text,
+                            }
+                        })}
+                    />
+                </Tabs.Tab>
+            </Tabs>
+
         </Card>
     );
 }
